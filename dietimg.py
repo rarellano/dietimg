caf1ff3a8297966191c7bc86e941aac24c7bd42c
rarellano <rafa.are.ro@gmail.com>
2017-01-25 19:21:13 +0100

    add set user, group and permission for files

--- dietimg.py
+++ dietimg.py
@@ -1,8 +1,11 @@
-#!/usr/bin/python3.5
+#!/usr/bin/python
 
 import os,sys
 
-MAX_FILE_SIZE = 300
+max_file_size = 300 # EXAMPLE: max_file_size = 300 (kilobytes) 
+user = ''           # EXAMPLE: user = 'www-data'
+group = ''          # EXAMPLE: group = 'www-data'
+permissions = ''    # EXAMPLE: permissions = '644' (in octal nomenclature)
 
 if len(sys.argv) > 1:
     workDir = sys.argv[1]
@@ -13,5 +16,7 @@ imgs = os.listdir(workDir)
 
 for img in imgs:
     size = os.path.getsize(os.path.join(workDir, img))
-    if size > (MAX_FILE_SIZE*1024):
+    if size > (max_file_size*1024):
         os.system('mogrify -quality 30% ' + img)
+        os.system('chmod ' + user + ':' + group + ' ' + img)
+        os.system('chmod ' + permissions + img)
